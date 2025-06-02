4639766b1623a99a283b4acede17728cae94f34f
APorkolab <adam.porkolab@gmail.com>
2025-06-02 05:55:43 +0200

    feat(frontend): shared components index

---
+++ frontend/src/app/shared/components/index.ts
@@ -0,0 +1,16 @@
+// Shared components barrel export
+export { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+export { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+export { ToastContainerComponent } from './toast-container/toast-container.component';
+export { ErrorStateComponent, ErrorType } from './error-state/error-state.component';
+export { ModalComponent } from './modal/modal.component';
+
+// Form components
+export { BaseFormComponent } from './base-form/base-form.component';
+export { FormFieldComponent } from './form-field/form-field.component';
+
+// Data display components
+export { DataTableComponent, TableColumn, SortEvent } from './data-table/data-table.component';
+
+// Skeleton components
+export * from './skeletons';
