946bf25a722c370146d2de4d16c88a6286c33adb
APorkolab <adam.porkolab@gmail.com>
2025-06-12 22:54:46 +0200

    test(frontend): purchase report tests

---
+++ frontend/src/app/page/purchase-report/purchase-report.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { PurchaseReportComponent } from './purchase-report.component';
+
+describe('PurchaseReportComponent', () => {
+  let component: PurchaseReportComponent;
+  let fixture: ComponentFixture<PurchaseReportComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PurchaseReportComponent],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PurchaseReportComponent);
+    component = fixture.componentInstance;
+    
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
