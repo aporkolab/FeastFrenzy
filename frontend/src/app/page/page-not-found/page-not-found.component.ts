d63a492b0020618d9adff27d3baba4b14953f36d
APorkolab <adam.porkolab@gmail.com>
2025-06-13 17:02:03 +0200

    feat(frontend): 404 not found page

---
+++ frontend/src/app/page/page-not-found/page-not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink]
+})
+export class PageNotFoundComponent {
+
+}
