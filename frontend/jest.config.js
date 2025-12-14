9e0919c147e0f7a284a54f5630bc5114f4c146dd
APorkolab <adam.porkolab@gmail.com>
2025-12-14 06:17:46 +0100

    feat(frontend): add lazy loading with custom preloading strategy
    Feature modules, HasRoleDirective, shared components/pipes, path aliases.
    Old routes redirect to new structure. 120 tests green.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
+  coverageProvider: 'v8',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
@@ -18,6 +19,14 @@ module.exports = {
   ],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@features/(.*)$': '<rootDir>/src/app/features/$1',
+    '^@models/(.*)$': '<rootDir>/src/app/model/$1',
+    '^@services/(.*)$': '<rootDir>/src/app/service/$1',
+    '^@guards/(.*)$': '<rootDir>/src/app/guards/$1',
+    '^@env/(.*)$': '<rootDir>/src/environments/$1',
     '@angular/core/testing': '<rootDir>/node_modules/@angular/core/fesm2022/testing.mjs',
     '@angular/common/http/testing': '<rootDir>/node_modules/@angular/common/fesm2022/http-testing.mjs',
     '@angular/common/http': '<rootDir>/node_modules/@angular/common/fesm2022/http.mjs',
